54fef07cbe09a397cb312fde0b85b5ff22271b28
Akbar Ali <akbaraliuetm136@gmail.com>
2026-04-12 20:58:58 +0500

    udpate the image name

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -74,7 +74,7 @@ export default function Footer() {
         {/* Right - Image (showing left side of person) */}
         <div className="footer-image-wrapper">
           <img 
-            src="/frontend/public/Akbarali.png" 
+            src="/Akbaralisame.png" 
             alt="Akbar Ali - Full Stack Developer available for freelance projects and collaborations" 
             className="footer-image"
             style={{borderRadius: '15px'}}
